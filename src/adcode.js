c4c4eb3b08eb923ff4c1b26eb7643d9b0290f3ec
yanghi <yanghi-x@outlook.com>
2021-04-30 19:20:59 +0800

    feat: adcode 工具

---
+++ src/adcode.js
@@ -0,0 +1,17 @@
+// 获取上一级行政区adcode仅精确到乡镇
+export const getUpDisByAdcode = (adcode) => {
+  const provinceCode = parseInt(adcode.slice(0, 2))
+  const cityCode = parseInt(adcode.slice(2, 2))
+  const streetCode = parseInt(adcode.slice(4, 6))
+
+  if (streetCode) {
+    return provinceCode + '' + cityCode + '00'
+  } else if (cityCode) {
+    return provinceCode + '0000'
+  } else {
+    return '100000'
+  }
+}
+export const getDepthByAdcode = (c) => {
+  return (c + '').replace('00', '').replace('00', '').length / 2
+}
