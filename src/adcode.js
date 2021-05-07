def21398a4d53f445eba8083422705244feeb575
yanghi <yanghi-x@outlook.com>
2021-05-07 22:55:24 +0800

    fix: 获取邮政编码城市部分错误

--- src/adcode.js
+++ src/adcode.js
@@ -1,7 +1,7 @@
 // 获取上一级行政区adcode仅精确到乡镇
 export const getUpDisByAdcode = (adcode) => {
   const provinceCode = parseInt(adcode.slice(0, 2))
-  const cityCode = parseInt(adcode.slice(2, 2))
+  const cityCode = parseInt(adcode.slice(2, 4))
   const streetCode = parseInt(adcode.slice(4, 6))
 
   if (streetCode) {
